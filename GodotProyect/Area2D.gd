f33a2c8d0ef68db6d11233985a41d4562ad7785d
VVizard22 <dandmc02@gmail.com>
2020-07-20 17:09:23 -0300

    WaitForCollisionToPickUp

--- GodotProyect/Area2D.gd
+++ GodotProyect/Area2D.gd
@@ -3,7 +3,7 @@ extends Area2D
 onready var nombre = self.get_parent().name
 onready var textura = self.get_parent().texture
 
-
+var clicked = false
 var hover = false
 
 func _on_mouse_entered():
@@ -12,7 +12,14 @@ func _on_mouse_entered():
 func _on_mouse_exited():
 	hover = false
 
+func _process(delta):
+	if clicked:
+		var areas = self.get_overlapping_areas()
+		for s in areas:
+			if s.get_parent().is_in_group("Player"):
+				Inventario.added_item(nombre, textura)
+				self.get_parent().queue_free()
+
 func _input(event):
 	if hover and event.is_action_pressed("left_click"):
-		Inventario.added_item(nombre, textura)
-		self.get_parent().queue_free()
+		clicked = true
