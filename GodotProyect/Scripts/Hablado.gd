0d4c4c31d71d86489419d59c9e7512a2ebc01c67
Chomei <tommbenitez16@gmail.com>
2020-07-25 20:20:33 -0300

    Timer

--- GodotProyect/Scripts/Hablado.gd
+++ GodotProyect/Scripts/Hablado.gd
@@ -1,8 +1,16 @@
 extends Label
 
+onready var timer = get_parent().get_node("Timer")
+
 func _ready():
 	Dialogo.connect("topico", self, "decir")
 	
 func decir(topic):
+	timer.set_wait_time(5)
+	timer.start()
 	self.text = "-" + Dialogo.descripcion[topic]
 	get_parent().get_node("Efecto").play("Typewriter_text_effect")
+
+
+func _on_Timer_timeout():
+	self.text = ""
