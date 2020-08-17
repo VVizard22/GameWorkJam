a9031d492274ba6fe8300238a2c7237d035f295d
Chomei <tommbenitez16@gmail.com>
2020-08-17 15:51:49 -0300

    bugfix

--- GodotProyect/Scripts/MainMenu.gd
+++ GodotProyect/Scripts/MainMenu.gd
@@ -4,11 +4,8 @@ extends Control
 func play_pressed():
 	Loader._changeScene("Tutorial", "Wipe")
 
-
-func options_pressed():
-	pass
-	#get_tree().change_scene_to(options_scn)
-
-
 func _on_Salir_pressed():
 	get_tree().quit()
+
+func _on_Creditos_pressed():
+	Loader._changeScene("ArtRoom", "Wipe")
