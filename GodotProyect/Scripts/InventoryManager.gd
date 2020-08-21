6e36485a73814b58d58103790028f878520bfdd7
Chomei <tommbenitez16@gmail.com>
2020-08-21 20:42:01 -0300

    organizacion?)

--- GodotProyect/Scripts/InventoryManager.gd
+++ GodotProyect/Scripts/InventoryManager.gd
@@ -9,6 +9,12 @@ func _on_Area2D_mouse_entered():
 func _on_Area2D_mouse_exited():
 	hover = false
 
+func deleteSlot(slot):
+	slot.queue_free()
+	
+func newSlot():
+	load("ress://Scenes/Inventario/Slot.tscn")
+
 func _ready():
 	Inventario.connect("nuevoInventario", self, "_on_nuevoInventario")
 	Inventario.connect("combineItems", self, "_on_combineItems")
@@ -20,7 +26,8 @@ func _on_nuevoInventario():
 		slots[useSlot].get_child(0).get_child(0).texture = Inventario.inventario[key]
 
 func _on_combineItems():
-	for item in slots:
-		if Inventario.inventario.keys().find(item) == -1:
-			item.name = "Slot"
-			item.texture = null
+	for key in Inventario.inventario:
+		var useSlot = Inventario.inventario.keys().find(key)
+		if useSlot == -1:
+			deleteSlot(useSlot)
+			newSlot()
